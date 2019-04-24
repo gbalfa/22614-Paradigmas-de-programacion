4235b87a99ff103f84ee36f728dafbac0b433967
Gabriel Badilla <gabriel.badilla.alfaro@gmail.com>
2019-04-23 23:12:28 -0400

    Documentación y formato.

--- labAbr05.py
+++ labAbr05.py
@@ -1,51 +1,84 @@
-def factorial (n):
+""" Argumentos en mayúsculas corresponden a listas.
+"""
+
+
+def factorial(n):
+    """ Función que retorna el valor de n factorial.
+    """
     return 1 if n <= 0 else n*factorial(n-1)
 
-def potencia (n,m):
-    return 1 if m <= 0 else n*potencia(n,m-1)
+
+def potencia(n, m):
+    """ Función que retorna el valor de n elevado a la m.
+    """
+    return 1 if m <= 0 else n*potencia(n, m-1)
+
 
 def qsort(L):
-    if L == []: return []
-    return  qsort([x for x in L[1:] if x< L[0]]) + L[0:1] + \
-            qsort([x for x in L[1:] if x>=L[0]])
+    """Función que ordena una lista de menor a mayor usando el algoritmo Quick Sort.
+    """
+    if L == []:
+        return []
+    return qsort([x for x in L[1:] if x < L[0]]) + L[0:1] + \
+        qsort([x for x in L[1:] if x >= L[0]])
+
 
 def fibonacci(n):
-    E=[]
+    """Función que retorna una lista con los primeros n números de Fibonacci.
+    """
+    E = []
     for i in range(n):
         if i < 8:
-            E+=[N_Fib(i+1)]
+            E += [N_Fib(i+1)]
         else:
-            E+=[E[len(E)-2]+E[-1]]
+            E += [E[len(E)-2]+E[-1]]
     return E
 
+
 def N_Fib(n):
-    if n <= 1: return 0
-    if n == 2: return 1
+    """ Función que retorna el número n de la sucesión de Fibonacci.
+    """
+    if n <= 1:
+        return 0
+    if n == 2:
+        return 1
     if n > 2:
         return N_Fib(n-1)+N_Fib(n-2)
 
+
 def maximo(L):
-    m = None
-    for e in L:
-         if m is None or m < e:
-             m = e
-    return m
+    """ Dada una lista retorna el elemento con el valor máximo.
+    """
+    max = None
+    for elem in L:
+        if max is None or max < elem:
+            max = elem
+    return max
+
 
 def minimo(L):
-    m = None
-    for e in L:
-         if m is None or m > e:
-             m = e
-    return m
+    """ Dada una lista retorna el elemento con el valor mínimo.
+    """
+    min = None
+    for elem in L:
+        if min is None or min > elem:
+            min = elem
+    return min
+
 
 def impares(L):
-    for e in L:
-        if not e%2:
-            L.remove(e)
+    """ Dada una lista de números, elimina los pares.
+    """
+    for elem in L:
+        if not elem % 2:
+            L.remove(elem)
     return L
 
+
 def pares(L):
-    for e in L:
-        if e%2:
-            L.remove(e)
+    """ Dada una lista de números, elimina los impares.
+    """
+    for elem in L:
+        if elem % 2:
+            L.remove(elem)
     return L
